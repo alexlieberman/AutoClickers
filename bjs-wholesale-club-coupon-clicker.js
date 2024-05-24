3eaefa44e72ba46d8e77297dd299e4f8269b71e8
Alex Lieberman <lexlieberman@gmail.com>
2024-05-24 11:27:00 -0400

    Update bjs-wholesale-club-coupon-clicker.js

--- bjs-wholesale-club-coupon-clicker.js
+++ bjs-wholesale-club-coupon-clicker.js
@@ -1,53 +1,66 @@
 // ==UserScript==
 // @name              BJ's Wholesale Club Coupon Clicker
 // @namespace         bjs-wholesale-coupon
-// @version           1.2.8
+// @version           1.4
 // @description       Click all the coupons on BJ's Wholesale Club website
 // @author            sleevetrick
-// @match             https://www.bjs.com/myCoupons
-// @match             https://www.bjs.com/myCoupons?source=header
-// @contributionURL   https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=abl34@cornell.edu&item_name=Greasy+Fork+Donation
+// @match             https://www.bjs.com/myCoupons*
 // ==/UserScript==
+// Donations not required, but gladly appreciated via https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=lexlieberman@gmail.com&item_name=Greasy+Fork+for+BJs+CC
  
-var couponAttribute = "";
-var totalCoupon = 0;
+// Track total clippings between loads
+var total_clipped = 0;
  
-function clipCoupons(index, elements, limit, count){
+// Conduct coupon clipping
+function clipCoupons(current_coupon_index, coupon_element_list){
  
-   if (elements == "undefined") {
-      elements = document.querySelectorAll(".gray-btn");
-      count = elements.length;
-      clipCoupons(0, elements, 1000, count);
-      console.log("Next Page...");
+    if (total_clipped === undefined) {
+        total_clipped = 0;
+    }
  
-   } else if (index < count){
-       elements[index].click();
-       totalCoupon++;
-       index++;
-       console.log("Clipping Coupon #" + totalCoupon);
-       setTimeout(()=>{
-           clipCoupons(index, elements, 1000, count);
-       }, 750)
-   } else {
-      if(document.querySelectorAll("span[class='next']").length >= 1) {
-        document.querySelectorAll("span[class='next']")[0].click();
-        setTimeout(()=>{
-               clipCoupons(0, "undefined", 1000, 0);
-       }, 3000)
+    // Retrieve the maximum count of coupons
+    var maximum_count_of_coupons = document.querySelectorAll('[data-auto-data="coupon_categoryFilteredCouponCnt"]')[0].lastChild.innerHTML.replaceAll(" Coupons","");
+ 
+    // Auto refresh the page if there are issues with auto-scrolling
+    if (current_coupon_index > maximum_count_of_coupons || current_coupon_index > 40) {
+        window.scrollTo(0, 0);
+        location.href = "https://www.bjs.com/myCoupons" + "?clipped=" + total_clipped;
+    }
+ 
+    // Select all the coupons that still need clipping and scroll a bit
+    coupon_element_list = document.querySelectorAll('[data-auto-data="coupon_ClipToCard"]');
+    window.scrollBy(0,80);
+ 
+   // If there are no coupons left to clip
+   if (maximum_count_of_coupons == 0) {
+ 
+       if (current_coupon_index > 0) {
+          alert("You auto-clipped at least ["+total_clipped+"] coupons! Please consider a donation: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=lexlieberman@gmail.com&item_name=Greasy+Fork+for+BJs+CC");
       } else {
-        printEnd();
+          console.log("No coupons to clip");
       }
-   }
+   } else if (coupon_element_list == "undefined") {
+      alert("Alert developer to fix due to BJ's code change");
+   } else {
+       coupon_element_list[0].click();
+       total_clipped++;
+       current_coupon_index++;
+       console.log("Clipping Coupon #" + total_clipped);
+       setTimeout(()=>{
+           clipCoupons(current_coupon_index, coupon_element_list);
+       }, 1000)
+   } 
 }
  
+// Loads the initial coupon clipping
 function loadFrame() {
-    totalCoupon = 0;
+    total_clipped = parseInt(new URLSearchParams(window.location.search).get('clipped'));
+    if (isNaN(total_clipped)) {
+        total_clipped = 0;
+    }
     console.log('Beginning Coupon Clipping...!');
-    clipCoupons(0, "undefined", 1000, 0);
+    clipCoupons(0, "undefined");
 };
  
+// After the window loads, begin loading the coupon clipping
 window.onload = setTimeout(loadFrame, 5000);
- 
-function printEnd(){
-   console.log('Finished');
-}
